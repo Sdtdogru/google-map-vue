2037faddc1bedfcebc05d6db32d98269bff9e767
Sedat Doğru <46866955+Sdtdogru@users.noreply.github.com>
2022-05-30 16:22:48 +0300

    axios request and places list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,81 @@
 <template>
   <div class="ui grid">
     <div class="six wide column">
-    <form class="ui segment large form">
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui right icon input large">
-            <input type="text" placeholder="Enter your address"  />
-            <i class="dot circle link icon "></i>
+      <form class="ui segment large form">
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address" v-model="coordinates" />
+              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+            </div>
+          </div>
+          <div class="field">
+            <div class="two fields">
+              <div class="field">
+                <input type="text" placeholder="Enter Radius Km" v-model="radius" />
+              </div>
+            </div>
+          </div>
+          <div class="ui primary button" tabindex="0" @click="findCloseBuyButtonPressed">
+            Search
           </div>
         </div>
-        <div class="field">
-          <div class="two fields">
-            <div class="field">
-              <input type="text" placeholder="Enter Radius Km"  />
+      </form>
+      <div class="ui segment" style="max-height:500px;overflow:scroll">
+        <div class="ui divided items">
+          <div class="item" v-for="place in places" :key="place.id">
+            <div class="content">
+              <div class="header">{{ place.name }}</div>
+              <div class="meta">{{ place.vicinity }}</div>
             </div>
           </div>
         </div>
-        <button class="ui primary button" >Search</button>
       </div>
-    </form>
-    <div class="ui segment"  style="max-height:500px;overflow:scroll">
-   
-</div>
-</div>
+    </div>
     <div class="ten wide column segment ui"></div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'HelloWorld',
-  props: {
-    
+  data() {
+    return {
+      lat: 0,
+      lng: 0,
+      radius: "",
+      places: []
+    };
+  },
+  computed: {
+    coordinates() {
+      return `${this.lat}, ${this.lng}`;
+    }
+  },
+  methods: {
+    locatorButtonPressed() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+        }
+      );
+    },
+    findCloseBuyButtonPressed() {
+      console.log("burda");
+      const URL = `http://localhost:8070/nearby-search/${this.lat}/${this.lng}/${this.radius * 1000}`;
+      axios.get(URL).then(response => {
+        this.places = response.data.data;
+        console.log("sdfds " + response.data);
+        this.initMap();
+      }).catch(error => {
+        console.log(error.message);
+      });
+    },
+    initMap() {
+    }
+
   }
 }
 </script>
